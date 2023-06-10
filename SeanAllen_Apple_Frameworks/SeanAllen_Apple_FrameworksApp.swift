1afe5f59ac92e3266e46177076a70695429df303
Batson Seales <batson.seales@gmail.com>
2023-06-10 10:15:45 -0500

    Initial Commit

---
+++ SeanAllen_Apple_Frameworks/SeanAllen_Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeanAllen_Apple_FrameworksApp.swift
+//  SeanAllen_Apple_Frameworks
+//
+//  Created by Batson Seales on 6/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SeanAllen_Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
